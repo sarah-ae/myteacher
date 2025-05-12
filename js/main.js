b12611bfb3889b6641b4f4d3684c1aa6dde9169a
WYSIWYG <82746890+sarah-ae@users.noreply.github.com>
2025-05-12 11:05:17 +0800

    font

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ window.onload = function () {
         content.salutation = result.salutation;
         content.signature = result.signature;
         content.body = result.body;
-        content.sign = getPureStr(content.signature).pxWidth('18px Satisfy, cursive');
+        content.sign = getPureStr(content.signature).pxWidth('10px Satisfy, cursive');
         document.title = result.title;
         $('#recipient').append(result.recipient);
         $('#flipback').text(result.sender);
